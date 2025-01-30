d4c32186e8c4726d016afec82d93ec8078532af5
LucianoBnf <luciano.novo.filho@gmail.com>
2025-01-30 18:27:35 -0300

    Exercício de wrapper

---
+++ Tarefa_classes/src/Tarefa_Wrapper/exemplo1.java
@@ -0,0 +1,15 @@
+package Tarefa_Wrapper;
+
+public class exemplo1 {
+
+    public static void main(String args[]) {
+        int num = 250;
+        System.out.println(num);
+
+        Integer num1 = Integer.valueOf(250);
+        num = num1;
+        System.out.println(num);
+
+
+    }
+}
