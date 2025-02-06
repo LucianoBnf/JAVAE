cb0c4556bde9f113abe4f7d91b6a06350f13ef6e
LucianoBnf <luciano.novo.filho@gmail.com>
2025-02-06 16:21:20 -0300

    aa

--- Tarefa_classes/src/Modulo12/Parte1.java
+++ Tarefa_classes/src/Modulo12/Parte1.java
@@ -1,27 +1,20 @@
-package Modulo12;
-
-import java.util.Arrays;
 import java.util.Scanner;
-
 import static java.util.Arrays.sort;
 
 public class Parte1 {
-
     public static void main(String[] args) {
-
         String resposta;
         String[] nomes;
-
         Scanner s = new Scanner(System.in);
-
-        System.out.println("Escreva todos os nomes desejados separados por , ");
+        System.out.println("Escreva todos os nomes desejados separados por ,");
         resposta = s.nextLine();
-
         nomes = resposta.split(",");
-        sort(nomes);
-
         for (int i = 0; i < nomes.length; i++) {
-            System.out.println(nomes[i]);
+            nomes[i] = nomes[i].trim(); // remove espaços em branco
+        }
+        sort(nomes);
+        for (String nome : nomes) {
+            System.out.println(nome);
         }
     }
 }
